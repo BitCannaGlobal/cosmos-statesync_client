2505a9f3d0289aaa0de7130f6d9ae5ba5118c67e
El-Jay_Crypto <laurensnijkamp@hotmail.com>
2024-04-17 14:02:28 +0200

    Update statesync_DEVNET-1_client_darwin_arm64.sh

--- statesync_DEVNET-1_client_darwin_arm64.sh
+++ statesync_DEVNET-1_client_darwin_arm64.sh
@@ -19,7 +19,7 @@ FILE=$(which jq)
 set -e
 
 # Change for your custom chain
-BINARY="https://github.com/BitCannaGlobal/bcna/releases/download/v3.0.2-rc1/bcna_darwin_arm64.tar.gz"
+BINARY="https://github.com/BitCannaGlobal/bcna/releases/download/v3.1.0-rc2/bcna_linux_amd64.tar.gz"
 GENESIS="https://raw.githubusercontent.com/BitCannaGlobal/bcna/main/devnets/bitcanna-dev-1/genesis.json"
 APP="BCNA: ~/.bcna"
 echo ".....DEVNET-1 use only........"
